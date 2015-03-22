89ebdd4dace35bcc1a6f10c6da6bcaf2929ead7f
Christian <christian.revillard@centraliens.net>
2015-03-22 22:45:45 +0100

    Restructure applications

--- Client/js/chat.js
+++ Client/js/chat.js
@@ -3,8 +3,8 @@ ChatController = {};
 $(document).ready(function(){
 	var socket = 
 		window.location.href.indexOf('rhcloud.com')>-1?
-	    io("http://nodejs-creweb.rhcloud.com:8000/chat"):
-		io("/chat");
+	    io("http://nodejs-creweb.rhcloud.com:8000/chat/chatSocket"):
+		io("/chat/chatSocket");
 	  
 	ChatController.submitMessage = function(){
 	    socket.emit('chat message', $('#m').val());
