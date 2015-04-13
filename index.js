9576e1f4840e4e6e957a07952cdbe9037c49bfd1
Christian <chrrevil@hotmail.com>
2015-04-13 18:27:51 +0200

    Working stand alone Chat

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ var server = require('cre-nodejs-server');
 
 var handlers = [];
 
-handlers = server.addDefaultHandlers(
-  require('path').resolve(__dirname), // root 
-  handlers,                           // only defaults
+handlers = server.addDefaultHandlers(  
+  handlers,                     // only defaults
   '/Chat/Client/html/chat.html' // Start page
 );
 
-server.server.start(handlers);
+server.server.start(
+  require('path').resolve(__dirname), // root 
+  handlers);
